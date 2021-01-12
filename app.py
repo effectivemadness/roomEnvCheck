0e2a3a26a5257aeab01cd851912739bae494137a
seungmin lee <madcat@khu.ac.kr>
2021-01-12 23:41:25 +0900

    create csv file when server starts

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 
+currentTime = datetime.now()
+timeText = currentTime.strftime("%Y%m%d_%H:%M:%S")
+fileName = timeText+'.csv'
+with open(fileName,'a') as file_data:
+        print('time,temp,humid',file=file_data)
+
 app = Flask(__name__)
 @app.route('/update', methods=['POST'])
 def update():
@@ -9,12 +15,12 @@ def update():
     currentTime = datetime.now()
     timeText = currentTime.strftime("%Y%m%d_%H:%M:%S")
     tempHumidText = str(content['temp'])+', '+str(content['humid'])
-    with open('data.csv','a') as file_data:
+    with open(fileName,'a') as file_data:
         print(timeText+', '+tempHumidText,file=file_data)
     return timeText+', '+tempHumidText
 @app.route('/')
 def home():
-    with open('data.csv','r') as file_data:
+    with open(fileName,'r') as file_data:
         lines = file_data.read().splitlines()
         last_line = lines[-1]
     timeText, tempText, humidText = last_line.split(', ')
